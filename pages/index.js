68d6246fe836f77566e90b324ecfcc0ba7cf90ff
Gökhan Demirel <e.gokhandemirel@hotmail.com>
2023-03-06 22:42:02 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { GoogleMap, useLoadScript, Autocomplete, Marker } from '@react-google-maps/api';
+
+const libraries = ["places"];
+
+export default function Places() {
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDzbccBtk5s7cvKxf-MlNFVIM0z468EWPo",
+    libraries: libraries
+  });
+
+  const center = { lat: 41.015137, lng: 28.979530 };
+
+  return (
+    <>
+      {isLoaded && (
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+        >
+          <Marker position={center} />
+          <Autocomplete  >
+            <input type="text" placeholder="Search..." className="map-autocomplete" />
+          </Autocomplete>
+        </GoogleMap>
+      )}
+    </>
+  );
+}
